0b1d4b0c85275f016293a1930ac2c950d9e7b346
Arthur Araujo <arthur.araujo@tutanota.com>
2023-07-26 10:43:32 -0300

    refactor: remove redundant variable declaration

--- src/main/java/com/newplans/api/security/Token.java
+++ src/main/java/com/newplans/api/security/Token.java
@@ -28,13 +28,11 @@ public class Token {
 	}
 	
 	private char[] getPossibleDigits() {
-		char[] possibleHexDigits = {
+		return new char[]{
 			'0', '1', '2', '3', '4', '5',
 			'6', '7', '8', '9', 'C', 'Z',
 			'c', 'd', 'e', 'f', 'b', 'a',
 			'H', 'B', 'F', 'M', 'L', 'R'
 		};
-		
-		return possibleHexDigits;
 	}
 }
